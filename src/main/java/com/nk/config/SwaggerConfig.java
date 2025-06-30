a0dbd857640b43762cb2d6e9205570685cfb78c9
Neeraj <neerajk.azad@gmail.com>
2025-06-30 19:25:41 +0530

    Added Swagger

---
+++ src/main/java/com/nk/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.nk.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IRCTC REST API")
+                        .version("1.0")
+                        .description("This is the IRCTC API documentation using OpenAPI and Swagger UI."));
+    }
+}
